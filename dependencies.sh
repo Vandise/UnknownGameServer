c657307bad49b470a901d3b9db42ce21add9db86
Benjamin J Anderson <banderson@mashable.com>
2016-05-30 10:47:06 -0500

    Switch dependency installation order

--- dependencies.sh
+++ dependencies.sh
@@ -3,13 +3,17 @@
 GIT_USER="Vandise"
 DEPENDENCIES=("ConnectServer" "LoginServer")
 
+echo "Installing Dependencies: "
+
 for repo in "${DEPENDENCIES[@]}"
 do
   git clone "https://github.com/$GIT_USER/$repo"
   cd "$repo/"
   echo "Current Directory:"
   pwd
+  echo "Installing Package Dependencies: "
   npm install
+  echo "Running $repo as Background Process"
   npm start &
   cd "../"
 done
