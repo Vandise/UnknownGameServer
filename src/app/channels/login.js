a90f7059b7be2d6f939c44c3464393832f10c464
Benjamin J Anderson <banderson@mashable.com>
2016-05-29 21:45:10 -0500

    Send user data to client after successful login

--- src/app/channels/login.js
+++ src/app/channels/login.js
@@ -2,14 +2,11 @@ export default (server, session) => {
   let socket = session.socket;
 
   socket.on('login_attempt', (data) => {
-    console.log('Attempting to login: ');
-    console.log(data);
     server.lsSocket.emit('login', data);
   });
 
   server.lsSocket.on('login', (data) => {
-    console.log('Login Server login resp: ');
-    console.log(data);
+    socket.emit('login_attempt', data);
   });
   
 };
